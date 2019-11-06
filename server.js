10809cf18c23a4b44555feb2245aa7e5b74d3d2d
Tiago Silva <tiago.silva@impactzero.pt>
2019-11-06 17:49:51 +0000

    chat

--- server.js
+++ server.js
@@ -14,28 +14,64 @@ app.use('/', (req, res) => {
     res.render('index.html');
 });
 
+server.listen(3000);
+
+let users = {};
 let messages = [];
-let users = [];
 
-io.on('connection', socket => {
-    console.log(`Socket Connected: ${socket.id}`);
 
-    socket.emit('previousMessages', messages);
+io.sockets.on('connection', (socket) => {
 
-    socket.on('sendMessage', data => {
-        messages.push(data);
-        socket.broadcast.emit('receivedMessage', data);
+    socket.on('newUser', (data, callback) => {
+        if(data in users) {
+            callback(false, data);
+        }
+        else {
+            callback(true, data);
+            socket.user = data;
+            users[socket.user] = socket;
+            updateUsers();
+        }
     });
 
-    socket.on('sendUser', user => {
-        users.push(user);
+    socket.on('previousMessages', (callback) => {
+        callback(messages);
     });
 
-    setInterval(function() {
-        socket.broadcast.emit('getUsers', users);
-    }, 500);
+    socket.on('sendMessage', (data, callback) => {
+        let message = data.trim();
+        messages.push({message: message, user: socket.user});
 
-    socket.emit('getUsers', users);
+        if(data.substr(0, 3) === '/w ') {
+            let msg = message.substr(3);
+            let idx = msg.indexOf(' ');
+            if(idx !== -1) {
+                let receiver = msg.substr(0, idx);
+                msg = msg.substr(idx + 1);
+                if(receiver in users) {
+                    users[receiver].emit('privateMessage', {message: msg, user: socket.user});
+                }
+                else {
+                    callback(`Please, enter a valid user to start a private message!`);
+                }
+            }
+            else {
+                callback(`Please, enter a valid message to start a private message!`);
+            }
+        }
+        else {
+            io.sockets.emit('newMessage', {message: data, user: socket.user});
+        }
+    });
+
+    socket.on('disconnect', (data) => {
+        if(!socket.user) return;
+        delete users[socket.user];
+        updateUsers();
+    });
+
+    function updateUsers() {
+        io.sockets.emit('users', Object.keys(users));
+    }
 });
 
-server.listen(3000);
